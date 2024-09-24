48544935227a8f9af7676dc0eee61f7fdeeb0059
admin <mdyusufbinnur@gmail.com>
2024-09-24 22:04:08 +0600

    Bkash Payment API

--- database/migrations/2024_09_10_233703_create_bkash_token_managers_table.php
+++ database/migrations/2024_09_10_233703_create_bkash_token_managers_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('bkash_token_managers', function (Blueprint $table) {
             $table->id();
+            $table->string('id_token');
+            $table->string('token_type');
+            $table->string('expires_in');
+            $table->string('statusCode');
+            $table->string('statusMessage');
+            $table->string('refresh_token');
             $table->timestamps();
         });
     }
